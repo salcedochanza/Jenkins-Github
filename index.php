98bb8cfabc1a07b049de8d20b6520cd727294678
salcedochanza <72934619+salcedochanza@users.noreply.github.com>
2021-04-15 16:03:16 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+$googleApiUrl = "http://api.openweathermap.org/data/2.5/weather?q=Lleida&appid=cdc8225afaa584c0dbf32b57f6b0ed0e&units=metric";
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_URL, $googleApiUrl);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_VERBOSE, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$response = curl_exec($ch);
+
+curl_close($ch);
+$data = json_decode($response);
+echo "La temperatura de lleida es: " . $data->main->temp . " ºC";
+?>
